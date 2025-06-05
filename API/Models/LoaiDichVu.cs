a0f90d72b194c823e4269e2511fa7a33d3af547e
nguyenlocx <nguyenle08101961@gmail.com>
2025-06-05 12:00:55 +0700

    quan ly dich vu

---
+++ API/Models/LoaiDichVu.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class LoaiDichVu
+    {
+        [Key]
+        public int LoaiDichVuID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string TenLoai { get; set; }
+
+        // Navigation property: Một loại dịch vụ có nhiều dịch vụ
+        public ICollection<DichVu>? DichVus { get; set; }
+    }
+}
